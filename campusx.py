73796de37e0e2dd16c79f72afa48523332de99d6
shriju <76461089+shriju@users.noreply.github.com>
2023-02-01 20:11:00 -0500

    Add files via upload

---
+++ campusx.py
@@ -0,0 +1,39 @@
+# open google.com
+# search campusx
+# learnwith.campusx.in
+# dsmp course page
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+s = Service("C:/Users/shrij/OneDrive/Desktop/chromedriver.exe")
+
+driver = webdriver.Chrome(service=s)
+driver.get('http://google.com')
+time.sleep(2)
+
+# fetch the search engine box using xpath
+user_input = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+user_input.send_keys('learnCampusx')
+time.sleep(2)
+
+user_input.send_keys(Keys.ENTER)
+time.sleep(1)
+
+link = driver.find_element(by=By.XPATH, value='//*[@id="rso"]/div[1]/div/div/div[1]/div/a')
+link.click()
+
+time.sleep(1)
+
+link2 = driver.find_element(by=By.XPATH, value='/html/body/div[1]/header/section[2]/a[5]')
+link2.click()
+
+time.sleep(2)
+
+
+
+
+
